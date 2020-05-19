f8e39ebd3823687e3a91f130cee44d1db372c94a
Juan Hamilton <hedwards507@gmail.com>
2020-05-19 16:38:14 -0500

    added options menu

--- root/js/script.js
+++ root/js/script.js
@@ -2,6 +2,9 @@ const original = document.body.innerHTML;
 const selectDifficulty = document.getElementById('js--selectDifficulty');
 const easyMode = document.getElementById('js--easyMode');
 const optionsMenu = document.getElementById('js--optionsMenu');
+const h2 = document.querySelector('.heading-secondary');
+
+let menu;
 
 document.addEventListener('click', changePage);
 document.addEventListener('click', toggleMenu);
@@ -13,12 +16,6 @@ function changePage(e) {
     if(element.id === 'js--classic') {
         document.body.innerHTML = selectDifficulty.innerHTML;
     }
-    else if(element.id ==='js--arrow') {
-        // document.body.innerHTML = original;
-        // document.body.removeAttribute('style');
-        optionsMenu.style.display = 'none';
-        console.log('works');
-    }
     else if(element.id === 'js--easy') {
         document.body.innerHTML = easyMode.innerHTML;
         document.body.style.backgroundImage = 'none';     
@@ -29,11 +26,11 @@ function toggleMenu(e) {
     const element = e.target;
 
     if(element.id === 'js--options') {
-        document.body.append(optionsMenu.content);
-        optionsMenu.style.display = 'block';
+        document.body.appendChild(optionsMenu.content);
+        menu = document.querySelector('.options-menu');
+        menu.style.display = 'block';
     }
-}
-
-function off() {
-    document.querySelector('.options').style.display = 'none';
+    else if(element.id === 'js--arrow') {
+        element.parentElement.parentElement.parentElement.parentElement.style.display = 'none';
+    }
 }
