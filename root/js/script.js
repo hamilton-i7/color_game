7c2e00bd5c0439fd8994789b4d2ab8dec8efc813
Juan Hamilton <hedwards507@gmail.com>
2020-05-19 23:37:35 -0500

    fixed arrow functionality

--- root/js/script.js
+++ root/js/script.js
@@ -1,10 +1,12 @@
 const original = document.body.innerHTML;
 const selectDifficulty = document.getElementById('js--selectDifficulty');
 const easyMode = document.getElementById('js--easyMode');
-const optionsMenu = document.getElementById('js--optionsMenu');
+const optionsMenu = document.querySelector('.options-menu');
 const h2 = document.querySelector('.heading-secondary');
 
 let menu;
+let options;
+let arrow;
 
 document.addEventListener('click', changePage);
 document.addEventListener('click', toggleMenu);
@@ -15,10 +17,16 @@ function changePage(e) {
 
     if(element.id === 'js--classic') {
         document.body.innerHTML = selectDifficulty.innerHTML;
+        arrow = document.querySelector('.icon-arrow');
+        arrow.setAttribute('id', 'arrowDifficulty');
     }
     else if(element.id === 'js--easy') {
         document.body.innerHTML = easyMode.innerHTML;
-        document.body.style.backgroundImage = 'none';     
+        document.body.style.backgroundImage = 'none';
+        options = document.querySelector('#js--options');
+    }
+    else if(element.id === 'arrowDifficulty') {
+        reset();
     }
 }
 
@@ -26,19 +34,27 @@ function toggleMenu(e) {
     const element = e.target;
 
     if(element.id === 'js--options') {
-        if(document.body.contains(menu)) {
-            menu = document.querySelector('.options-menu');
-            menu.style.display = 'block';
-        }
-        else {
-            document.body.appendChild(optionsMenu.content);  
-            menu = document.querySelector('.options-menu');
-            menu.style.display = 'block'; 
-        }
+        displayOptionsMenu();
     }
-    else if(element.id === 'js--arrow' || element.classList.contains('heading-secondary') || element.id === 'icon-arrow') {
-        menu = document.querySelector('.options-menu');
-        menu.style.display = 'none';
-        console.log('works');
+    else if(element.parentElement.innerText === 'Options') {
+        optionsMenu.style.display = 'none';
+    }
+    else if(element.id === 'js--exit') {
+        reset();
+    }
+}
+
+function reset() {
+    document.body.innerHTML = original;
+    document.body.removeAttribute('style');
+}
+
+function displayOptionsMenu() { 
+    if(document.body.contains(optionsMenu)) {
+        optionsMenu.style.display = 'block';
+    }
+    else {
+        document.body.appendChild(optionsMenu);
+        optionsMenu.style.display = 'block'; 
     }
 }
