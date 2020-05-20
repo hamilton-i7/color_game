5092cb7876379e811643286689b2d7147ebfb451
Juan Hamilton <hedwards507@gmail.com>
2020-05-20 15:46:38 -0500

    added content

--- root/js/script.js
+++ root/js/script.js
@@ -7,13 +7,22 @@ const h2 = document.querySelector('.heading-secondary');
 
 let menu;
 let options;
-let arrow;
 let helpContent;
+let arrow;
+
+let box1;
+let box2;
+let box3;
+
+let lifeCount;
 
 document.addEventListener('click', changePage);
 document.addEventListener('click', toggleOptionsMenu);
 document.addEventListener('click', toggleHelp);
+document.addEventListener('click', checkAnswer);
+document.addEventListener('click', gameOver);
 
+//* NAVIGATION
 
 function changePage(e) {
     const element = e.target;
@@ -27,6 +36,9 @@ function changePage(e) {
         document.body.innerHTML = easyMode.innerHTML;
         document.body.style.backgroundImage = 'none';
         options = document.querySelector('#js--options');
+
+        setPrompt();
+        changeBoxColor();
     }
     else if(element.id === 'arrowDifficulty') {
         reset();
@@ -43,7 +55,7 @@ function toggleOptionsMenu(e) {
         exitOptionsMenu();
     }
     else if(element.id === 'js--exit') {
-        reset();
+        quitGame();
     }
 }
 
@@ -58,7 +70,7 @@ function toggleHelp(e) {
     }
 }
 
-function reset() {
+function quitGame() {
     document.body.innerHTML = original;
     document.body.removeAttribute('style');
 }
@@ -95,4 +107,86 @@ function displayHelpInfo() {
 function exitHelpInfo() {
     document.body.style.overflowY = 'scroll';
     help.style.display = 'none';
+}
+
+
+//* GAME LOGIC
+
+function changeBoxColor(e) {
+    box1 = document.getElementById('js--box1');
+    box2 = document.getElementById('js--box2');
+    box3 = document.getElementById('js--box3');
+
+    const prompt = document.querySelector('.heading-primary').innerText.toLowerCase();
+    const answer = prompt;
+    const boxes = [box1, box2, box3];
+    const max = boxes.length - 1;
+    const min = 0;
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    boxes[random].style.backgroundColor = answer;
+
+    for(let i = 0; i < boxes.length; i++) {
+        if(boxes[i] === boxes[random]) {
+            boxes[i].style.backgroundColor = answer;
+        }
+        else {
+            boxes[i].style.backgroundColor = `rgb(${randomNum(0, 255)})`;
+        }
+    } 
+}
+
+function randomNum(min, max) {
+    let numbers = [];
+
+    for(let i = 0; i < 3; i++) {
+        numbers.push(Math.floor(Math.random() * max) + min);
+    }
+
+    return numbers;
+}
+
+function setPrompt() {
+    const prompt = document.querySelector('.heading-primary');
+    prompt.innerText = `rgb(${randomNum(0, 255)[0]}, ${randomNum(0, 255)[1]}, ${randomNum(0, 255)[2]})`;
+}
+
+function checkAnswer(e) {
+    const element = e.target;
+    const answer = document.querySelector('.heading-primary').innerText.toLowerCase();
+
+    if(element.style.backgroundColor === answer) {
+        setPrompt();
+        changeBoxColor();
+        reset();
+    }
+    else if(element.classList.contains('game__card') && element.style.backgroundColor !== answer) {
+        element.style.visibility = 'hidden';
+        loseLife();
+        // gameOver();
+    }
+}
+
+function reset() {
+    box1 = document.getElementById('js--box1');
+    box2 = document.getElementById('js--box2');
+    box3 = document.getElementById('js--box3');
+    const boxes = [box1, box2, box3];
+
+    boxes.forEach(function(box) {
+        box.style.visibility = 'visible';
+    });
+}
+
+function loseLife() {
+    lifeCount = document.getElementById('js--lives');
+    const currentLife = Number(lifeCount.innerText.split(' ')[1]);
+    const newLife = currentLife - 1;
+    const result = lifeCount.innerText.replace(currentLife, newLife);
+    
+    return lifeCount.innerText = result;
+}
+
+function gameOver(e) {
+    const element = e.target;
 }
